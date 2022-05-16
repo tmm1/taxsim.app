e67ed19de60888896b16a14f8a217d7914fac964
Aman Karmani <aman@tmm1.net>
2022-05-16 15:46:19 -0700

    ignore tiny state amounts

--- src/App.vue
+++ src/App.vue
@@ -50,13 +50,13 @@ const stateVars = [
   {
     name: 'v34',
     label: 'Standard Deduction',
-    if: '$output.v34 > $output.v35 && $output.v32 > 0',
+    if: '$output.v34 > $output.v35 && $output.v32 >= 1',
     neg: true,
   },
   {
     name: 'v35',
     label: 'Itemized Deductions',
-    if: '$output.v35 > $output.v34 && $output.v32 > 0',
+    if: '$output.v35 > $output.v34 && $output.v32 >= 1',
     neg: true,
   },
   {
@@ -93,7 +93,7 @@ function varsToRows(vars) {
         attrs: {
           class: 'text-left col-span-2',
         },
-        if: [`$output.${o.name} * 1 != 0`, o.if].filter(o => !!o).join(' && '),
+        if: [`$output.${o.name} * 1 >= 1`, o.if].filter(o => !!o).join(' && '),
         children: [
           {
             $el: 'span',
@@ -113,7 +113,7 @@ function varsToRows(vars) {
       },
       {
         $cmp: 'amount',
-        if: [`$output.${o.name} * 1 != 0`, o.if].filter(o => !!o).join(' && '),
+        if: [`$output.${o.name} * 1 >= 1`, o.if].filter(o => !!o).join(' && '),
         props: {
           class: 'text-right col-start-3',
           neg: o.neg,
